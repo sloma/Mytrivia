dbf7e80db53b90a659b42d37913ff6cbdab53d61
salim bouassida <sloma27@hotmail.com>
2015-04-10 16:46:55 -0700

    Assignment 1

---
+++ main.h
@@ -0,0 +1,16 @@
+#include <allegro5/allegro.h>
+
+string quest11 = "1. What programming language is used in this book?";
+char *char11 = (char*)quest11.c_str();
+
+string quest11a = "A. C ";
+char *char11a = (char*)quest11a.c_str();
+
+string quest11b = "B. Pascal ";
+char *char11b = (char*)quest11b.c_str();
+
+string quest11c = "C. C++  ";
+char *char11c = (char*)quest11c.c_str();
+
+string quest11d = "D. Assembly ";
+char *char11d = (char*)quest11d.c_str();
